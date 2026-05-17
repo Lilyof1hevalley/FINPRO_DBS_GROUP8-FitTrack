b6cf4284588e49333a3613dd339740f5f10a2c5e
Nadira Fayyaza Aisy <nfa190506@gmail.com>
2026-05-18 05:21:24 +0700

    updating api after i deploy backend

--- Frontend/src/api/client.js
+++ Frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://finprodbsgroup8-fittrack-production.up.railway.app/api',
   headers: { 'Content-Type': 'application/json' },
 })
 
